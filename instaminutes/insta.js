ce1fc28338b267be558f7fd1c6db0ed30ad50647
Vikash-Rai <you@example.com>
2022-03-30 10:34:41 +0530

    commit all

--- instaminutes/insta.js
+++ instaminutes/insta.js
@@ -1,22 +1,40 @@
 import schedule from 'node-schedule'
+import fs from 'fs'
 //At a particular date and time
-// const someDate = new Date('2022-03-29 13:53:00.000')
+// const someDate = new Date('2022-03-29 16:37:00.000')
 // schedule.scheduleJob(someDate,()=>{
 //     console.log("Job ran at",new Date().toString())
 // })
 
+fs.readFile("data.json", "utf8", (err, jsonString) => {
+    if (err) {
+        console.log("Error while reading from file", err);
+        return;
+    }
+////////////////////////////////////////CODE BLOCK////////////////////////////////////////////////////////////////////
+    try {
 
-//At recurrence intervals
-schedule.scheduleJob('my-job','*/2 * * * * *',()=>{
- console.log("I ran")
- schedule.cancelJob('my-job')
+        const myevent = JSON.parse(jsonString);
+        console.log("For each event item, hitting NodeJS scheduler API asking it to schedule the event... ")
+        myevent.events.forEach(job => {
+            schedule.scheduleJob(job.dateTime, async () => {
+                console.log("Event scheduled at =>",job.dateTime)
+                rev(job.text)
+            });
+        })
+        function rev(newtext) {
+            var length = newtext.length
+            newtext = [...newtext].reverse().join("");
+            setTimeout(() => {
+                console.log(`After sleeping for ${length} seconds `)
+                console.log("returning text : ", newtext)
+            }, length * 1000)
+        }
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    }
+    catch (err) {
+        console.log("error", err)
+    }
 })
-// *    *    *    *    *    *
-// ┬    ┬    ┬    ┬    ┬    ┬
-// │    │    │    │    │    │
-// │    │    │    │    │    └ day of week (0 - 7) (0 or 7 is Sun)
-// │    │    │    │    └───── month (1 - 12)
-// │    │    │    └────────── day of month (1 - 31)
-// │    │    └─────────────── hour (0 - 23)
-// │    └──────────────────── minute (0 - 59)
-// └───────────────────────── second (0 - 59, OPTIONAL)
